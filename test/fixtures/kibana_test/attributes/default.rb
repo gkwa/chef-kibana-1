6ad6d4244815f0ac7a1747fd752c9513b869d324
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-01-15 13:35:52 -0800

    Start testing remotely, wifi sucks

--- test/fixtures/kibana_test/attributes/default.rb
+++ test/fixtures/kibana_test/attributes/default.rb
@@ -3,3 +3,5 @@ default['java']['jdk_version'] = '7'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 default['kibana']['legacy_mode'] = false
 default['elasticsearch']['version'] = '2.1.0'
+default['kibana']['install_type'] = 'git'
+default['elasticsearch']['checksums']['5.1.2']['debian'] = '411091695ff9188b9394816f88f3328f478c4d21e2a80ce194660ee14b868475'
--- test/fixtures/kibana_test/recipes/default.rb
+++ test/fixtures/kibana_test/recipes/default.rb
@@ -1,5 +1,14 @@
 
-include_recipe 'elasticsearch::default'
+elasticsearch_user 'elasticsearch' do
+  action :nothing
+end
+
+elasticsearch_install 'elasticsearch' do
+  type "package"
+  version "5.1.2"
+  action :install
+end
+
 include_recipe 'kibana_lwrp::install'
 
 elasticsearch_service 'elasticsearch' do
