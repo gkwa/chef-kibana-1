464408529b9eb3266220fead268bd2866cb57f4c
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-01-19 00:05:52 -0800

    Kibana 5.1.2
    Check "branches" on https://github.com/elastic/kibana

--- test/fixtures/kibana_test/attributes/default.rb
+++ test/fixtures/kibana_test/attributes/default.rb
@@ -5,3 +5,4 @@ default['kibana']['legacy_mode'] = false
 default['elasticsearch']['version'] = '2.1.0'
 default['kibana']['install_type'] = 'file'
 default['elasticsearch']['checksums']['5.1.2']['debian'] = '411091695ff9188b9394816f88f3328f478c4d21e2a80ce194660ee14b868475'
+default['kibana']['version'] = '5.1.2-linux-x64'
