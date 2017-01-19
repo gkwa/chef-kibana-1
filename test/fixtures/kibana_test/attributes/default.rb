acce681be6ef63c19c212b44c660a1eb93719fea
Taylor Monacelli <taylor.monacelli@streambox.com>
2017-01-19 01:08:26 -0800

    fix version

--- test/fixtures/kibana_test/attributes/default.rb
+++ test/fixtures/kibana_test/attributes/default.rb
@@ -5,6 +5,6 @@ default['kibana']['legacy_mode'] = false
 default['elasticsearch']['version'] = '2.1.0'
 default['kibana']['install_type'] = 'file'
 default['elasticsearch']['checksums']['5.1.2']['debian'] = '411091695ff9188b9394816f88f3328f478c4d21e2a80ce194660ee14b868475'
-default['kibana']['version'] = '5.1.2-linux-x64'
+default['kibana']['version'] = '5.1.2-linux-x86_64'
 default['kibana']['file']['url'] = 'https://artifacts.elastic.co/downloads/kibana/kibana-5.1.2-linux-x86_64.tar.gz'
 default['kibana']['file']['checksum'] = 'c2e30b9581e7222e8f2536d4b08087dc282a6b31a24ec0e43b905507fe2f2b04'
